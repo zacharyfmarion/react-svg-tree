ed3e79936fb8ff711e708d1c145a6f4a3ad49af4
Zachary Marion <zfm2@duke.edu>
2018-08-03 22:33:42 -0700

    Remove docz from git

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'src/components/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-index" */ 'src/components/index.mdx'),
-  'src/components/Tree/Tree.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-tree-tree" */ 'src/components/Tree/Tree.mdx'),
-}

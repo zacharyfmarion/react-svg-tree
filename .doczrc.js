421790a3d4e87c1d0084e3d5fc8387b46ec9e586
Zachary Marion <zfm2@duke.edu>
2018-08-05 21:53:35 -0700

    Docz still not working

--- .doczrc.js
+++ .doczrc.js
@@ -1,4 +1,12 @@
 export default {
   typescript: true,
   port: 9001,
+  modifyBundlerConfig: (config, dev, args) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    });
+    return config;
+  },
 };

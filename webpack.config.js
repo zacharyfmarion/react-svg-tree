0c0463d798c42b701dfe5391f85fb18f9e4b6f91
Zachary Marion <zfm2@duke.edu>
2018-08-07 20:22:07 -0700

    Correctly bundle components

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,36 @@ module.exports = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM',
+    },
   },
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    library: {
+      root: 'ReactSvgTree',
+      amd: 'react-svg-tree',
+      commonjs: 'react-svg-tree',
+    },
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true,
   },
 };
